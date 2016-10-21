5785b4a1ef2a89515bfa9ca9ba4df86d62aa15cd
G-story <ksg2732@naver.com>
2016-10-21 15:57:29 +0900

    관광 정보 관련 model 구현

--- public_data_collector/models.py
+++ public_data_collector/models.py
@@ -1,7 +1,4 @@
-from datetime import datetime
-
 from django.db import models
-from django.utils import timezone
 
 
 class District(models.Model):
@@ -17,17 +14,22 @@ class Area(District):
 
 
 class Sigungu(District):
-    area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    area = models.ForeignKey(Area, on_delete=models.PROTECT)
 
     class Meta:
         unique_together = ('area', 'code')
 
 
 class SmallArea(District):
-    sigungu = models.ForeignKey(Sigungu, on_delete=models.CASCADE)
+    sigungu = models.ForeignKey(Sigungu, on_delete=models.PROTECT)
 
     class Meta:
         unique_together = ('sigungu', 'code')
+
+
+class ContentType(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
 
 
 class Category(models.Model):
@@ -43,11 +45,92 @@ class Category1(Category):
 
 
 class Category2(Category):
-    cat1 = models.ForeignKey(Category1, on_delete=models.CASCADE)
+    cat1 = models.ForeignKey(Category1, on_delete=models.PROTECT)
 
 
 class Category3(Category):
-    cat2 = models.ForeignKey(Category2, on_delete=models.CASCADE)
+    cat2 = models.ForeignKey(Category2, on_delete=models.PROTECT)
+
+
+class TravelInfo(models.Model):
+    addr1 = models.CharField(max_length=100)
+    addr2 = models.CharField(max_length=100, null=True, blank=True)
+    area_code = models.IntegerField()
+    sigungu_code = models.IntegerField()
+    sigungu = models.ForeignKey(Sigungu, on_delete=models.PROTECT)
+    contenttype = models.ForeignKey(ContentType, on_delete=models.PROTECT)
+    cat1_code = models.CharField(max_length=20)
+    cat2_code = models.CharField(max_length=20)
+    cat3_code = models.CharField(max_length=20)
+    cat3 = models.ForeignKey(Category3, on_delete=models.PROTECT)
+    title = models.CharField(max_length=100)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    thumbnail = models.CharField(max_length=200, null=True, blank=True)
+    mapx = models.FloatField()
+    mapy = models.FloatField()
+    mlevel = models.IntegerField()
+    is_book_tour = models.BooleanField()
+    tel = models.CharField(max_length=20, null=True, blank=True)
+    readcount = models.IntegerField()
+    created = models.DateTimeField(null=True, blank=True)
+    modified = models.DateTimeField(null=True, blank=True)
+
+
+class AdditionalInfo(models.Model):
+    travel_info = models.OneToOneField(TravelInfo, on_delete=models.PROTECT, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class OverviewInfo(AdditionalInfo):
+    telname = models.CharField(max_length=50, null=True, blank=True)
+    homepage = models.CharField(max_length=200, null=True, blank=True)
+    overview = models.CharField(max_length=5000, null=True, blank=True)
+
+
+class TourspotIntroInfo(AdditionalInfo):
+    accomcount = models.CharField(max_length=50, null=True, blank=True)
+    chkbabycarriage = models.CharField(max_length=20, null=True, blank=True)
+    chkcreditcard = models.CharField(max_length=20, null=True, blank=True)
+    chkpet = models.CharField(max_length=20, null=True, blank=True)
+    expagerange = models.CharField(max_length=50, null=True, blank=True)
+    expguide = models.CharField(max_length=1000, null=True, blank=True)
+    heritage1 = models.BooleanField(default=False)
+    heritage2 = models.BooleanField(default=False)
+    heritage3 = models.BooleanField(default=False)
+    infocenter = models.CharField(max_length=200, null=True, blank=True)
+    opendate = models.CharField(max_length=200, null=True, blank=True)
+    parking = models.CharField(max_length=500, null=True, blank=True)
+    restdate = models.CharField(max_length=200, null=True, blank=True)
+    useseason = models.CharField(max_length=200, null=True, blank=True)
+    usetime = models.CharField(max_length=200, null=True, blank=True)
+
+
+class CulturalFacilityIntroInfo(AdditionalInfo):
+    accomcount = models.CharField(max_length=50, null=True, blank=True)
+    chkbabycarriage = models.CharField(max_length=20, null=True, blank=True)
+    chkcreditcard = models.CharField(max_length=20, null=True, blank=True)
+    chkpet = models.CharField(max_length=20, null=True, blank=True)
+    discountinfo = models.CharField(max_length=500, null=True, blank=True)
+    infocenter = models.CharField(max_length=200, null=True, blank=True)
+    parking = models.CharField(max_length=500, null=True, blank=True)
+    parkingfee = models.CharField(max_length=500, null=True, blank=True)
+    restdate = models.CharField(max_length=200, null=True, blank=True)
+    usefee = models.CharField(max_length=200, null=True, blank=True)
+    usetime = models.CharField(max_length=200, null=True, blank=True)
+    scale = models.CharField(max_length=100, null=True, blank=True)
+    spendtime = models.CharField(max_length=200, null=True, blank=True)
+
+
+class DefaultDetailInfo(AdditionalInfo):
+    infoname = models.CharField(max_length=50, null=True, blank=True)
+    infotext = models.CharField(max_length=2000, null=True, blank=True)
+
+
+class ImageInfo(AdditionalInfo):
+    originimgurl = models.CharField(max_length=200, null=True, blank=True)
+    smallimageurl = models.CharField(max_length=200, null=True, blank=True)
 
 
 class Progress(models.Model):
@@ -72,8 +155,8 @@ class AreaCodeProgress(Progress):
     TOTAL_AREA_CNT = 17
 
     level = models.IntegerField(choices=LEVELS, default=NO)
-    area = models.OneToOneField(Area, null=True)
-    sigungu = models.OneToOneField(Sigungu, null=True)
+    area = models.OneToOneField(Area, null=True, on_delete=models.PROTECT)
+    sigungu = models.OneToOneField(Sigungu, null=True, on_delete=models.PROTECT)
     area_complete_count = models.IntegerField(default=0)
 
 
@@ -91,7 +174,6 @@ class CategoryCodeProgress(Progress):
     TOTAL_CATEGORY_CNT = 7
 
     level = models.IntegerField(choices=LEVELS, default=NO)
-    cat1 = models.OneToOneField(Category1, null=True)
-    cat2 = models.OneToOneField(Category2, null=True)
+    cat1 = models.OneToOneField(Category1, null=True, on_delete=models.PROTECT)
+    cat2 = models.OneToOneField(Category2, null=True, on_delete=models.PROTECT)
     cat1_complete_count = models.IntegerField(default=0)
-
